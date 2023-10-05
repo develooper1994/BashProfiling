7ab508b42ced4aaef086f6f47a25fa33eef7776c
Mustafa Selçuk Çağlar <11412546+develooper1994@users.noreply.github.com>
2023-10-05 10:41:48 +0300

    Update cpu_top_profiling.sh

--- cpu_top_profiling.sh
+++ cpu_top_profiling.sh
@@ -12,7 +12,7 @@ MAX_CPU=0
 #echo "    PID USER      PR  NI    VIRT    RES    SHR S  %CPU  %MEM     TIME+ COMMAND" > $LOG_FILE
 while true ; do
 
-    CPU_USAGE=($(top -n 1 -d .1 | grep $PID | sed -e 's/ \+/ /g' | awk '{print $(NF-4)}'))
+    CPU_USAGE=($(top -n 1 -d .1 | grep $PID | sed -e 's/ \+/ /g' | awk '{print $(NF-3)}'))
     if [[ -z $CPU_USAGE ]]; then
         echo "PID is not found. Closed or not running"
         exit 1
